e80382387520638ef0c385f90ac246f371ffcf58
byvide <byvide.x@gmail.com>
2024-08-02 20:57:23 +0200

    chore: implement the suggestion

--- src/hello.ts
+++ src/hello.ts
@@ -1,17 +1,12 @@
 /**
- * The function returns "Hello World!".
+ * The function returns Hello World!.
  * If a name is provided, it returns a personalized greeting message.
- * All whitespaces are trimmed. Empty string defaults to "Hello, World!".
+ * All whitespaces in the name are trimmed. An empty string or undefined name defaults to Hello, World!.
  *
- * @param {string} [name='World'] - The name of the person to greet.
+ * @param {string} [name] - The name of the person to greet. Optional.
  * @returns {string} The greeting message.
  */
-export function hello(name: string = 'World'): string {
-	const trimmedName = name.trim();
-
-	if (!trimmedName) {
-		return 'Hello World!';
-	}
-
-	return `Hello ${trimmedName}!`;
+export function hello(name?: string): string {
+	const trimmedName = name?.trim();
+	return trimmedName ? `Hello ${trimmedName}!` : 'Hello World!';
 }
